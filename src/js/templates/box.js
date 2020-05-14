873c849ac7ede998d0ff311e3840a9cc105131a1
Liam pop <liam@gotpop.net>
2020-05-15 00:18:13 +0100

    Cube face

--- src/js/templates/box.js
+++ src/js/templates/box.js
@@ -4,7 +4,16 @@ import { html } from 'lit-html';
 // Template
 const box = btn =>
 html`
-  <div class="box"><div>
+          <div class="scene">
+          <div class="cube show-front">
+            <div class="cube__face cube__face--front"></div>
+            <div class="cube__face cube__face--back"></div>
+            <div class="cube__face cube__face--right"></div>
+            <div class="cube__face cube__face--left"></div>
+            <div class="cube__face cube__face--top"></div>
+            <div class="cube__face cube__face--bottom"></div>
+          </div>
+        </div>
 `;
 
 export default box;

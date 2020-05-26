10155905d8725d8bf7752eb7277295c5cadc4ec4
gotpop <liam@gotpop.net>
2020-05-26 14:57:58 +0100

    Cleaning cards

--- src/js/cards.js
+++ src/js/cards.js
@@ -2,63 +2,72 @@
 export default class Cards {
   constructor() {
     this.html = {};
-    this.currentClass = "";
+    this.css = {};
     this.options = {
       fill: "forwards",
       duration: 400,
       iterations: 1,
     };
-    this.animateInEven = [{
-        transform: "rotateY(-30deg) translateX(-22vw)",
-        opacity: "0"
-      },
-      {
-        transform: "rotateY(0deg) translateZ(0)",
-        opacity: "1"
-      }
-    ];
-    this.animateInOdd = [{
-        transform: "rotateY(30deg) translateX(22vw)",
-        opacity: "0"
-      },
-      {
-        transform: "rotateY(0deg) translateZ(0)",
-        opacity: "1"
-      }
-    ];
-    this.animateOutEven = [{
-        transform: "rotateY(0deg) translateZ(0)",
-        opacity: "1"
-      },
-      {
-        transform: "rotateY(-30deg) translateX(-22vw)",
-        opacity: "0"
-      }
-    ];
-    this.animateOutOdd = [{
-        transform: "rotateY(0deg) translateZ(0)",
-        opacity: "1"
-      },
-      {
-        transform: "rotateY(30deg) translateX(22vw)",
-        opacity: "0"
-
-      }
-    ];
+    this.init();
+  }
+  
+  init() {
+    this.html.allCards = [...document.querySelectorAll(".cards__main .card")];
+    this.animations();
     this.observe();
   }
 
+  animations() {
+    this.css.animateInEven = [{
+        transform: "rotateY(-30deg) translateX(-22vw)",
+        opacity: "0"
+      },
+      {
+        transform: "rotateY(0deg) translateZ(0)",
+        opacity: "1"
+      }
+    ];
+    this.css.animateInOdd = [{
+        transform: "rotateY(30deg) translateX(22vw)",
+        opacity: "0"
+      },
+      {
+        transform: "rotateY(0deg) translateZ(0)",
+        opacity: "1"
+      }
+    ];
+    this.css.animateOutEven = [{
+        transform: "rotateY(0deg) translateZ(0)",
+        opacity: "1"
+      },
+      {
+        transform: "rotateY(-30deg) translateX(-22vw)",
+        opacity: "0"
+      }
+    ];
+    this.css.animateOutOdd = [{
+        transform: "rotateY(0deg) translateZ(0)",
+        opacity: "1"
+      },
+      {
+        transform: "rotateY(30deg) translateX(22vw)",
+        opacity: "0"
+      }
+    ];
+  }
+
   show(item, isEvenNumber) {
-  console.log('isEvenNumber :', isEvenNumber);
-    isEvenNumber ? item.animate(this.animateInEven, this.options) : item.animate(this.animateInOdd, this.options);
+    console.log('isEvenNumber :', isEvenNumber);
+    isEvenNumber ? item.animate(this.css.animateInEven, this.options) :
+      item.animate(this.css.animateInOdd, this.options);
   }
 
   hide(item, isEvenNumber) {
-    isEvenNumber ? item.animate(this.animateOutEven, this.options) : item.animate(this.animateOutOdd, this.options);
+    isEvenNumber ? item.animate(this.css.animateOutEven, this.options) :
+      item.animate(this.css.animateOutOdd, this.options);
   }
 
   observe() {
-    this.html.allCards = [...document.querySelectorAll(".cards__main .card")];
     const options = {
       threshold: 0.5
     };
@@ -68,7 +77,6 @@ export default class Cards {
         const isEvenNumber = ((i & 1) == 0);
         const isVisible = (entry.intersectionRatio > 0.5);
 
-        // isEvenNumber ? console.log('odd :', i, isEvenNumber) : console.log('even :', i, isEvenNumber);
         isVisible ? this.show(entry.target, isEvenNumber) : this.hide(entry.target, isEvenNumber);
       });
     };

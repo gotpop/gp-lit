51d50f15ec5d7d56d1f6fe1ab01ee181fde093d4
Liam <liam@gotpop.net>
2020-06-02 16:46:39 +0100

    Formatting

--- src/js/templates/cards.js
+++ src/js/templates/cards.js
@@ -1,14 +1,13 @@
-// Import 
+// Import
 import { html } from "lit-html";
 import cardsData from "../data/cards";
 import card from "./card";
 
 // Template
-const cards =
-    html `
-  <section class="cards">
+const cards = html`
+  <section class="cards" id="work">
     <div class="cards__main">
-        ${cardsData.map((carditem) => html`${card(carditem)}`)}
+      ${cardsData.map((carditem) => html`${card(carditem)}`)}
     </div>
   </section>
 `;
